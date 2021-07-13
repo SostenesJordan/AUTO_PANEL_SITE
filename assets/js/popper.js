c8ba78d9e3ba3673e810dbb7e3f74c8b42fcfda1
Sóstenes Jordan <sostenesj60@gmail.com>
2021-07-12 23:37:38 -0300

    alteracao de imagens

--- assets/js/popper.js
+++ assets/js/popper.js
@@ -1418,7 +1418,7 @@ function clockwise(placement) {
   var counter = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : false;
 
   var index = validPlacements.indexOf(placement);
-  var arr = validPlacements.slice(index + 1).concat(validPlacements.slice(0, index));
+  var arr = validPlacements.slice(index + 1 ).concat(validPlacements.slice(0, index));
   return counter ? arr.reverse() : arr;
 }
 
